f4e8cce4d2570d2a4858c0420d596973c80dffa4
Rachel Schirra <schirra.rachel@gmail.com>
2019-07-19 19:50:38 -0700

    Flask only likes it if it's a string.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_time():
-    return datetime.now().timestamp()
+    return str(datetime.now().timestamp())
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
